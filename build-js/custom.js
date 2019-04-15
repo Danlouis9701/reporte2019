a3f73bb00c9abe19d32a5847329726e1c49935ad
Dan <dan@studio-8.co>
2019-04-14 22:57:49 -0500

    portada, transformacion y valores terminados

--- build-js/custom.js
+++ build-js/custom.js
@@ -7,3 +7,22 @@ var pushbar = new Pushbar({
 });
 //close all pushbars
 pushbar.close();
+
+$(window).on("load", function () {
+    setTimeout('delay()', 2500);
+});
+
+function delay() {
+    $('#preloader').fadeOut("slow");
+    $('body').css({
+        'overflow': 'visible'
+    });
+    revapi42.revstart();
+}
+
+jQuery(document).ready(function ($) {
+    $('.counter').counterUp({
+        delay: 10,
+        time: 1000
+    });
+});
