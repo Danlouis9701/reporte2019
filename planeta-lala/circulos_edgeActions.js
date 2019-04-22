4c81877517b680cb8f7023781e7dbbb4441033eb
Dan <dan@studio-8.co>
2019-04-21 19:21:52 -0500

    Reporte terminado 21 de abril 19

---
+++ planeta-lala/circulos_edgeActions.js
@@ -0,0 +1,10 @@
+
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",119250,function(sym,e){sym.play(0);});
+//Edge binding end
+Symbol.bindTimelineAction(compId,symbolName,"Default Timeline","play",function(sym,e){sym.play(0);});
+//Edge binding end
+})("stage");
+//Edge symbol end:'stage'
+})(jQuery,AdobeEdge,"EDGE-140140569");
