df267ce8ad9afc01604bfc4ac503e7ceccc3fe3a
Alumno <alumno@v-9.local>
2019-08-09 18:52:59 -0500

    Version de Ingles V2.0

---
+++ en/build-js/custom.js
@@ -0,0 +1,28 @@
+new WOW().init();
+
+// Pushbar Js
+var pushbar = new Pushbar({
+    blur: true,
+    overlay: true
+});
+//close all pushbars
+pushbar.close();
+
+$(window).on("load", function () {
+    setTimeout('delay()', 3000);
+});
+
+function delay() {
+    $('#preloader').fadeOut("slow");
+    $('body').css({
+        'overflow': 'visible'
+    });
+    revapi42.revstart();
+}
+
+jQuery(document).ready(function ($) {
+    $('.counter').counterUp({
+        delay: 10,
+        time: 1000
+    });
+});
